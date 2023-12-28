faf66da8339f51229678320333897298417e4080
Bernd Schoolmann <mail@quexten.com>
2023-12-28 11:36:23 +0100

    Fix css not being loaded in flatpak

--- ui/settings.py
+++ ui/settings.py
@@ -295,8 +295,10 @@ def show_login():
     dialog.set_modal(True)
     dialog.present()
 
+isflatpak = os.path.exists("/.flatpak-info")
+pathprefix = "/app/bin/" if isflatpak else "./"
 css_provider = Gtk.CssProvider()
-css_provider.load_from_path("style.css")
+css_provider.load_from_path(pathprefix+"style.css")
 Gtk.StyleContext.add_provider_for_display(
     Gdk.Display.get_default(),
     css_provider,
