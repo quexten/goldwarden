1e24bce546ae71fc64c2b0e6a3c06de9bb89f8de
Bernd Schoolmann <mail@quexten.com>
2024-02-04 00:08:25 +0100

    Add windows ssh named pipe

--- agent/ssh/ssh.go
+++ agent/ssh/ssh.go
@@ -5,8 +5,6 @@ import (
 	"crypto/rand"
 	"errors"
 	"fmt"
-	"net"
-	"os"
 	"time"
 
 	"github.com/quexten/goldwarden/agent/config"
@@ -182,40 +180,5 @@ func NewVaultAgent(vault *vault.Vault, config *config.Config, runtimeConfig *con
 			log.Info("Unlock Request, but no action defined")
 			return false
 		},
-	}
-}
-
-func (v SSHAgentServer) Serve() {
-	path := v.runtimeConfig.SSHAgentSocketPath
-	if _, err := os.Stat(path); err == nil {
-		if err := os.Remove(path); err != nil {
-			log.Error("Could not remove old socket file: %s", err)
-			return
-		}
-	}
-	listener, err := net.Listen("unix", path)
-	if err != nil {
-		panic(err)
-	}
-
-	log.Info("SSH Agent listening on %s", path)
-
-	for {
-		var conn, err = listener.Accept()
-		if err != nil {
-			panic(err)
-		}
-
-		callingContext := sockets.GetCallingContext(conn)
-
-		log.Info("SSH Agent connection from %s>%s>%s \nby user %s", callingContext.GrandParentProcessName, callingContext.ParentProcessName, callingContext.ProcessName, callingContext.UserName)
-		log.Info("SSH Agent connection accepted")
-
-		go agent.ServeAgent(vaultAgent{
-			vault:               v.vault,
-			config:              v.config,
-			unlockRequestAction: v.unlockRequestAction,
-			context:             callingContext,
-		}, conn)
 	}
 }
