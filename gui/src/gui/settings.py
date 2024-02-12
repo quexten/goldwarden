9c840f3edcd951222c04c89c04546852ed84b455
Bernd Schoolmann <mail@quexten.com>
2024-02-12 05:15:43 +0100

    Attempt to fix settings freeze

--- gui/src/gui/settings.py
+++ gui/src/gui/settings.py
@@ -213,8 +213,6 @@ class SettingsWinvdow(Gtk.ApplicationWindow):
         self.preferences_group.add(self.status_row)
         
         def update_labels():
-            GLib.timeout_add(1000, update_labels)
-            
             pin_set = goldwarden.is_pin_enabled()
             status = goldwarden.get_vault_status()
             print("status", status)
@@ -275,6 +273,9 @@ class SettingsWinvdow(Gtk.ApplicationWindow):
                 if not is_daemon_running:
                     self.status_row.set_subtitle("Daemon not running")
                     self.vault_status_icon.set_icon("dialog-error", "error")
+            
+            GLib.timeout_add(5000, update_labels)
+
 
         GLib.timeout_add(1000, update_labels)
         self.set_default_size(400, 700)
