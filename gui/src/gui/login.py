6da52ba6dee73366f4ce32e6dfc93b6b5168b275
Bernd Schoolmann <mail@quexten.com>
2024-05-03 19:47:04 +0200

    Initial port to templates

---
+++ gui/src/gui/login.py
@@ -0,0 +1,49 @@
+import gi
+gi.require_version('Gtk', '4.0')
+gi.require_version('Adw', '1')
+import gc
+import time
+from gi.repository import Gtk, Adw, GLib, Notify, Gdk
+from threading import Thread
+from .template_loader import load_template
+import sys
+import os
+from ..services import goldwarden
+
+goldwarden.create_authenticated_connection(None)
+
+class GoldwardenLoginApp(Adw.Application):
+    def __init__(self, **kwargs):
+        super().__init__(**kwargs)
+        self.connect('activate', self.on_activate)
+
+    def on_activate(self, app):
+        self.load()
+        self.window.present()
+
+    def load(self):
+        builder = load_template("login.ui")
+        self.window = builder.get_object("window")
+        self.window.set_application(self)
+        self.email_row = builder.get_object("email_row")
+        self.client_id_row = builder.get_object("client_id_row")
+        self.client_secret_row = builder.get_object("client_secret_row")
+        self.server_row = builder.get_object("server_row")
+        self.login_button = builder.get_object("login_button")
+        self.login_button.connect("clicked", lambda x: self.on_login())
+        
+    def on_login(self):
+        email = self.email_row.get_text()
+        client_id = self.client_id_row.get_text()
+        client_secret = self.client_secret_row.get_text()
+        server = self.server_row.get_text()
+        goldwarden.set_url(server)
+        if client_id != "":
+            goldwarden.set_client_id(client_id)
+        if client_secret != "":
+            goldwarden.set_client_secret(client_secret)
+        goldwarden.login_with_password(email, "")
+
+if __name__ == "__main__":
+    app = GoldwardenLoginApp(application_id="com.quexten.Goldwarden.login")
+    app.run(sys.argv)
--- gui/src/gui/quickaccess.py
+++ gui/src/gui/quickaccess.py
@@ -7,172 +7,111 @@ import time
 from gi.repository import Gtk, Adw, GLib, Notify, Gdk
 from ..services import goldwarden
 from threading import Thread
+from .template_loader import load_template
 import sys
 import os
 from ..services import totp
 Notify.init("Goldwarden")
 
-# read line from stdin
-token = sys.stdin.readline()
-goldwarden.create_authenticated_connection(token)
-
-def autotype(text):
-    goldwarden.autotype(text)
-    time.sleep(0.1)
-    os._exit(0)
-
-def set_clipboard(text):
-    Gdk.Display.get_clipboard(Gdk.Display.get_default()).set_content(
-            Gdk.ContentProvider.new_for_value(text)
-        )
-
-    def kill():
-        time.sleep(0.5)
-        os._exit(0)
-    thread = Thread(target=kill)
-    thread.start()
-
-class MyApp(Adw.Application):
+class GoldwardenQuickAccessApp(Adw.Application):
     def __init__(self, **kwargs):
         super().__init__(**kwargs)
+        self.logins = []
+        self.filtered_logins = []
+        self.query = ""
         self.connect('activate', self.on_activate)
 
-    def update_logins(self):
-        logins = goldwarden.get_vault_logins()
-        if logins == None:
-            os._exit(0)
-            return
-        self.app.autofill_window.logins = logins
-
     def on_activate(self, app):
-        self.autofill_window = MainWindow(application=app)
-        self.autofill_window.logins = []
-        self.autofill_window.present()
-        self.app = app
+        self.load()
+        self.window.present()
         thread = Thread(target=self.update_logins)
         thread.start()
 
-class MainWindow(Gtk.ApplicationWindow):
-    def __init__(self, *args, **kwargs):
-        super().__init__(*args, **kwargs)
+    def load(self):
+        builder = load_template("quickaccess.ui")
+        self.window = builder.get_object("window")
+        self.results_list = builder.get_object("results_list")
+        self.status_page = builder.get_object("status_page")
+        self.text_view = builder.get_object("search_row")
+        self.text_view.connect("changed", self.on_type)
+        self.window.set_application(self)
 
-        self.stack = Gtk.Stack()
-        self.stack.set_transition_type(Gtk.StackTransitionType.SLIDE_LEFT_RIGHT)
-        self.set_child(self.stack)
+    def update(self):
+        self.update_list()
+        self.render_list()
 
-        self.box = Gtk.Box()
-        self.box.set_orientation(Gtk.Orientation.VERTICAL)
-        self.stack.add_named(self.box, "box")
+    def autotype(self, text):
+        goldwarden.autotype(text)
+        time.sleep(0.1)
+        os._exit(0)
 
-        self.text_view = Adw.EntryRow()
-        self.text_view.set_title("Search")
-    
-        def on_type(entry):
-            if len(entry.get_text()) > 1:
-                self.results_list.show()
-            else:
-                self.results_list.hide()
+    def set_clipboard(self, text):
+        Gdk.Display.get_clipboard(Gdk.Display.get_default()).set_content(
+            Gdk.ContentProvider.new_for_value(text)
+        )
 
+        def kill():
+            time.sleep(0.5)
+            os._exit(0)
+        thread = Thread(target=kill)
+        thread.start()
+
+    def update_list(self):
+        if self.query == "":
+            self.filtered_logins = []
+            return
+        
+        self.filtered_logins = list(filter(lambda i: self.query.lower() in i["name"].lower(), self.logins))
+
+        self.starts_with_logins = list(filter(lambda i: i["name"].lower().startswith(self.query.lower()), self.filtered_logins))
+        self.other_logins = list(filter(lambda i: i not in self.starts_with_logins, self.filtered_logins))
+        self.filtered_logins = self.starts_with_logins + self.other_logins
+        if len(self.filtered_logins) > 7:
+            self.filtered_logins = self.filtered_logins[0:7]
+
+    def render_list(self):
+        if len(self.filtered_logins) > 1:
+            self.results_list.set_visible(True)
             while self.results_list.get_first_child() != None:
                 self.results_list.remove(self.results_list.get_first_child())
+            self.status_page.set_visible(False)
+        else:
+            self.results_list.set_visible(False)
+            self.status_page.set_visible(True)
 
-            self.filtered_logins = list(filter(lambda i: entry.get_text().lower() in i["name"].lower(), self.logins))
-            if len( self.filtered_logins) > 10:
-                 self.filtered_logins =  self.filtered_logins[0:10]
-            self.starts_with_logins = list(filter(lambda i: i["name"].lower().startswith(entry.get_text().lower()), self.logins))
-            self.other_logins = list(filter(lambda i: i not in self.starts_with_logins ,  self.filtered_logins))
-            self.filtered_logins = None
+        for i in self.filtered_logins:
+            action_row = Adw.ActionRow()
+            action_row.set_title(i["name"])
+            action_row.set_subtitle(i["username"])
+            action_row.set_icon_name("dialog-password")
+            action_row.set_activatable(True)
+            action_row.password = i["password"]
+            action_row.username = i["username"]
+            action_row.uuid = i["uuid"]
+            action_row.uri = i["uri"]
+            action_row.totp = i["totp"]
+            self.results_list.append(action_row)
+        self.starts_with_logins = None
+        self.other_logins = None
 
-            for i in self.starts_with_logins  + self.other_logins :
-                action_row = Adw.ActionRow()
-                action_row.set_title(i["name"])
-                action_row.set_subtitle(i["username"])
-                action_row.set_icon_name("dialog-password")
-                action_row.set_activatable(True)
-                action_row.password = i["password"]
-                action_row.username = i["username"]
-                action_row.uuid = i["uuid"]
-                action_row.uri = i["uri"]
-                action_row.totp = i["totp"]
-                self.results_list.append(action_row)
-            self.starts_with_logins = None
-            self.other_logins = None
-        self.text_view.connect("changed", lambda entry: on_type(entry))
-        self.box.append(self.text_view)
+    def on_type(self, entry):
+        search_query = entry.get_text()
+        self.query = search_query
+        self.update()
     
-        self.results_list = Gtk.ListBox()
-        # margin'
-        self.results_list.set_margin_start(10)
-        self.results_list.set_margin_end(10)
-        self.results_list.set_margin_top(10)
-        self.results_list.set_margin_bottom(10)
-        self.results_list.hide()
-
-        keycont = Gtk.EventControllerKey()
-        def handle_keypress(controller, keyval, keycode, state, user_data):
-            ctrl_pressed = state & Gdk.ModifierType.CONTROL_MASK > 0
-            alt_pressed = state & Gdk.ModifierType.ALT_MASK > 0
-
-            if keycode == 9:
-                os._exit(0)
-
-            if keyval == 65364:
-                # focus results
-                if self.results_list.get_first_child() != None:
-                    self.results_list.get_first_child().grab_focus()
-                    self.results_list.select_row(self.results_list.get_first_child())
-
-            if keyval == 113:
-                return False
-
-            if keycode == 36:
-                self.hide()
-                autotypeThread = Thread(target=autotype, args=(f"{self.results_list.get_selected_row().username}\t{self.results_list.get_selected_row().password}",))
-                autotypeThread.start()
-            if keyval == 112:
-                print("pass", ctrl_pressed, alt_pressed)
-                if ctrl_pressed and not alt_pressed:
-                    set_clipboard(self.results_list.get_selected_row().password)
-                if ctrl_pressed and alt_pressed:
-                    self.hide()
-                    autotypeThread = Thread(target=autotype, args=(self.results_list.get_selected_row().password,))
-                    autotypeThread.start()
-            elif keyval == 117:
-                if ctrl_pressed and not alt_pressed:
-                    set_clipboard(self.results_list.get_selected_row().username)
-                if ctrl_pressed and alt_pressed:
-                    self.hide()
-                    autotypeThread = Thread(target=autotype, args=(self.results_list.get_selected_row().username,))
-                    autotypeThread.start()
-            elif keyval == 118:
-                if ctrl_pressed and alt_pressed:
-                    environment = goldwarden.get_environment()
-                    if environment == None:
-                        return
-                    item_uri = environment["vault"] + "#/vault?itemId=" + self.results_list.get_selected_row().uuid
-                    Gtk.show_uri(None, item_uri, Gdk.CURRENT_TIME)
-            elif keyval == 108:
-                if ctrl_pressed and alt_pressed:
-                    Gtk.show_uri(None, self.results_list.get_selected_row().uri, Gdk.CURRENT_TIME)
-            elif keyval == 116:
-                totp_code = totp.totp(self.results_list.get_selected_row().totp)
-                if ctrl_pressed and not alt_pressed:
-                    set_clipboard(totp_code)
-                if ctrl_pressed and alt_pressed:
-                    self.hide()
-                    autotypeThread = Thread(target=autotype, args=(totp_code,))
-                    autotypeThread.start()
-            elif keyval == 102:
-                # focus search
-                self.text_view.grab_focus()
-                
-        keycont.connect('key-pressed', handle_keypress, self)
-        self.add_controller(keycont)
-
-        self.results_list.get_style_context().add_class("boxed-list")
-        self.box.append(self.results_list)
-        self.set_default_size(700, 700)
-        self.set_title("Goldwarden Quick Access")
-
-app = MyApp(application_id="com.quexten.Goldwarden.autofill-menu")
-app.run(sys.argv)
+    def update_logins(self):
+        logins = goldwarden.get_vault_logins()
+        print(logins)
+        if logins == None:
+            os._exit(0)
+            return
+        self.logins = logins
+        self.update()
+    
+if __name__ == "__main__":
+    # todo add proper method to debug this
+    # token = sys.stdin.readline()
+    token = "Test"
+    goldwarden.create_authenticated_connection(token)
+    app = GoldwardenQuickAccessApp(application_id="com.quexten.Goldwarden.quickaccess")
+    app.run(sys.argv)
