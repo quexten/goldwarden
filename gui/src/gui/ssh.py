d960c8f122e956264fa1a313e60ce32691332f4e
Bernd Schoolmann <mail@quexten.com>
2024-05-03 22:43:28 +0200

    Add resource json

--- gui/src/gui/ssh.py
+++ gui/src/gui/ssh.py
@@ -8,7 +8,7 @@ gi.require_version('Adw', '1')
 from gi.repository import Gtk, Adw, GLib, Gdk, Gio
 from ..services import goldwarden
 from threading import Thread
-from .template_loader import load_template
+from .resource_loader import load_template, load_json
 import subprocess
 from . import components
 import os
@@ -26,6 +26,11 @@ class GoldwardenSSHSetupGuideApp(Adw.Application):
         builder = load_template("ssh.ui")
         self.window = builder.get_object("window")
         self.window.set_application(self)
+        commands = load_json("commands")
+        self.add_ssh_key_row = builder.get_object("add_ssh_key_row")
+        self.add_ssh_key_row.set_subtitle(commands["add-ssh-key"])
+        self.ssh_socket_path_row = builder.get_object("ssh_socket_path_row")
+        self.ssh_socket_path_row.set_subtitle(commands["ssh-socket-path"])
 
 if __name__ == "__main__":
     app = GoldwardenSSHSetupGuideApp(application_id="com.quexten.Goldwarden.sshsetup")
